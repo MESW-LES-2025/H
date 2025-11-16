2d3c3303ddc3b7d418cf2a12bb394e815ea41c3c
mariajvieira <up202204802@up.pt>
2025-11-16 17:54:54 +0000

    format code for consistency

--- backend/src/main/java/com/lernia/auth/dto/RegisterRequest.java
+++ backend/src/main/java/com/lernia/auth/dto/RegisterRequest.java
@@ -8,14 +8,14 @@ public class RegisterRequest {
 
     public RegisterRequest() {}
     public RegisterRequest(String name, String username, String password, String email) {
-        this.name=name;
+        this.name = name;
         this.username = username;
         this.password = password;
         this.email = email;
     }
 
-    public String getName() {return name;} 
-    public void setName(String name) { this.name=name;}
+    public String getName() { return name; } 
+    public void setName(String name) { this.name = name;}
     public String getUsername() { return username; }
     public void setUsername(String username) { this.username = username; }
     public String getPassword() { return password; }
