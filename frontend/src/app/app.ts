737de6d354e2633bdac87bc0d78684de6d34a2d8
Carolina Costa <acarolinacc2003@gmail.com>
2025-11-16 11:45:00 +0000

    Done NavBar

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [
-    RouterOutlet
-  ],
+  standalone: true,
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
 })
-export class App {
-  protected readonly title = 'Lernia';
-}
+export class AppComponent {}
