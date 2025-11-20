ec023c2e274f20b1d869151a3a7300b2a49111c9
Gessica Goulart <gessica.goulart@cgm.com>
2025-11-20 23:30:55 +0000

    feature/filter-implemenations: Updated relation between ScholarshipEntity and UniversityEntity.

--- backend/src/main/java/com/lernia/auth/entity/ScholarshipEntity.java
+++ backend/src/main/java/com/lernia/auth/entity/ScholarshipEntity.java
@@ -1,16 +1,17 @@
 package com.lernia.auth.entity;
 
-import lombok.Data;
-
+import com.lernia.auth.entity.enums.CourseType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "scholarships", schema = "lernia")
@@ -20,12 +21,12 @@ public class ScholarshipEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "university_id", nullable = false)
     private UniversityEntity university;
     private String name;
     private String description;
     private Integer amount;
     @Enumerated(EnumType.STRING)
-    private String courseType;
+    private CourseType courseType;
 }
