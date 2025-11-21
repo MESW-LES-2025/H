ca3b47f3f6a40b3eee431d4cb22f701a00d8f597
Gessica Goulart <gessica.goulart@cgm.com>
2025-11-21 17:39:45 +0000

    feature/filter-implemenations: Refactored ScholarshipEntity courseType from enum to string to comply with frontend. Validaton was added to make sure only valid enum values are persisted.

--- backend/src/main/java/com/lernia/auth/entity/ScholarshipEntity.java
+++ backend/src/main/java/com/lernia/auth/entity/ScholarshipEntity.java
@@ -1,15 +1,17 @@
 package com.lernia.auth.entity;
 
 import com.lernia.auth.entity.enums.CourseType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PostLoad;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -27,6 +29,15 @@ public class ScholarshipEntity {
     private String name;
     private String description;
     private Integer amount;
-    @Enumerated(EnumType.STRING)
-    private CourseType courseType;
+    @Column
+    private String courseType;
+
+    @PostLoad
+    @PrePersist
+    @PreUpdate
+    private void validateCourseType() {
+        if (!CourseType.contains(courseType)) {
+            throw new IllegalArgumentException("Invalid courseType: " + courseType);
+        }
+    }
 }
--- backend/src/main/java/com/lernia/auth/entity/enums/CourseType.java
+++ backend/src/main/java/com/lernia/auth/entity/enums/CourseType.java
@@ -1,3 +1,15 @@
 package com.lernia.auth.entity.enums;
 
-public enum CourseType { BACHELOR, MASTER, DOCTORATE }
+public enum CourseType {
+
+    BACHELOR, MASTER, DOCTORATE;
+
+    public static boolean contains(String value) {
+        try {
+            CourseType.valueOf(value);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
