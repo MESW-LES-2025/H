8bdedde45d17c4981d8a758d1e457d07babfd905
mariajvieira <up202204802@up.pt>
2025-12-02 21:04:52 +0000

    fixed course page

--- frontend/src/app/course-page/services/course-page-service.ts
+++ frontend/src/app/course-page/services/course-page-service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { CourseViewmodel } from '../viewmodels/course-viewmodel';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CoursePageService {
 
+  constructor(private http: HttpClient) {}
+
   public getCourseProfile(id: number): Observable<CourseViewmodel> {
-    return of();
+    return this.http.get<CourseViewmodel>(`${environment.apiUrl}/api/courses/${id}`);
   }
 }
